a888b6b6d04555b2438eaf498bc7d0b95c6bb120
danieltien13 <danieltien13@gmail.com>
2023-11-15 21:35:22 -0800

    working prototype

--- recipe_search.py
+++ recipe_search.py
@@ -3,6 +3,7 @@ from streamlit_gsheets import GSheetsConnection
 from sentence_transformers import SentenceTransformer
 import torch
 import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
 
 spreadsheet = "https://docs.google.com/spreadsheets/d/1oboE6E4MVZ538GilwB2Pvgm6249QxT6E8lkJzsL4yEk/edit#gid=0"
 
@@ -28,6 +29,32 @@ def presearch(df: pd.DataFrame) -> torch.Tensor:
     embeddings = model.encode(sentences)
     return embeddings
 
+@st.cache_data(ttl=3600*24*30)
+def create_search_embedding(sentence: str) -> torch.Tensor:
+    """
+    """
+    model = SentenceTransformer('sentence-transformers/all-mpnet-base-v2')
+    search_embedding = model.encode(sentence)
+    return search_embedding
+
+@st.cache_data(ttl=3600*24*30)
+def find_search_results(embeddings: torch.Tensor, search_embedding: torch.Tensor) -> pd.DataFrame:
+    """
+    
+    """
+    results = []
+    search_emb = search_embedding.reshape(1,-1)
+    for n_emb, embedding in enumerate(embeddings):
+        row = {}
+        emb = embedding.reshape(1,-1)
+
+        cos_sim = cosine_similarity(search_emb, emb)[0]
+        row["index"] = n_emb
+        row["cos_sim"] = cos_sim
+        results.append(row)
+        
+    return pd.DataFrame(results).sort_values(by="cos_sim", ascending=False)
+
 st.title("Recipe Search Engine :shallow_pan_of_food: :female-cook:")
 
 conn = st.connection("gsheets", type=GSheetsConnection)
@@ -35,19 +62,22 @@ conn = st.connection("gsheets", type=GSheetsConnection)
 df = conn.read()
 
 # just play around with 100 examples for now
-df = df.iloc[:100]
+df = df.iloc[:1000]
 
-text_search = st.text_input("Search for recipes")
+text_search = st.text_input("Search for your favorite recipes!")
 
 embeddings = presearch(df)
 
-st.write(df.head())
+# st.write(df.head())
 
 # st.write(embeddings)
 
-df_search = df
-
 if text_search:
+    search_embedding = create_search_embedding(text_search)
+    df_search = find_search_results(embeddings, search_embedding)
+
+    df_search = df_search.merge(df, left_index=True, right_index=True)
+
     for n_row, row in df_search.reset_index().iterrows():
         # draw a line if there is a new row
         if n_row%CARDS_PER_ROW == 0:
@@ -55,6 +85,9 @@ if text_search:
             cols = st.columns(CARDS_PER_ROW, gap="large")
         with cols[n_row%CARDS_PER_ROW]:
             st.write(f"{row['title']}")
-            st.write(f"{row['NER']}")
+            st.markdown("Ingredients: " + row['NER'][1:-1])
+            # for item in row['NER'][1:-1].split(","):
+            #     st.markdown(" - " + item.strip('"').strip())
             st.write(f"{row['link']}")
+            st.write(f"Score: {row['cos_sim'][0]:.2f}")
 
